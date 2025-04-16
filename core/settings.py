d2c6940f11bd12a05c7da02c0575b91a16c3a1e1
gabrieltacosta <gabriel.tuka@gmail.com>
2025-04-16 11:12:25 -0300

    update settings

--- core/settings.py
+++ core/settings.py
@@ -110,7 +110,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "templates/static"),)
 STATIC_ROOT = os.path.join("static")
 
@@ -143,7 +143,7 @@ JAZZMIN_SETTINGS = {
     },
 
     # Welcome text on the login screen
-    'welcome_sign': 'Bem-vindo(a) ao Consta QUE',
+    'welcome_sign': 'Bem-vindo(a) ao Inteligência do 6º BPRv',
 
     # Copyright on the footer
     'copyright': '| 6º BPRv - Inteligência',
